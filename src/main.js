4f1b1c746fc25464b999cdae7019706dbd006f29
NikSankovich <nik.sankovich@gmail.com>
2023-06-07 11:37:30 -0500

    capstone backend

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 let express = require('express');
-
+const cors = require("cors");
 let app = express();
 
 const port = process.env.PORT || 4002;
-
+app.use(cors());
 app.use(express.json());
-
+app.options("*", cors());
 let authRoutes = require('./routes/authRoutes');
 app.use('/', authRoutes);
 
